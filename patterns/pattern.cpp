4cbae58824b6c0173d7aaefb244fd7af421c0199
Laxmi Satanhalli <lsatenhalli@gmail.com>
2026-08-09 12:52:55 +0530

    add dsa

--- patterns/pattern.cpp
+++ patterns/pattern.cpp
@@ -51,18 +51,118 @@ void pattern6(int n){
 }
 void pattern7(int n){
  for(int i=0;i<=n;i++){
-    for(int j=0;j<n-i-1;j++){
+    for(int j=0;j<=n-i-1;j++){
       cout<<" ";
     }
     for(int j=0;j<2*i+1;j++){
       cout<<"*";
     }
-    for(int j=0;j<n-i-1;j++){
+    for(int j=0;j<=n-i-1;j++){
       cout<<" ";
     }
     cout<<endl;
   }
 }
+void pattern8(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern9(int n){
+for(int i=0;i<=n;i++){
+    for(int j=0;j<=n-i-1;j++){
+      cout<<" ";
+    }
+    for(int j=0;j<2*i+1;j++){
+      cout<<"*";
+    }
+    for(int j=0;j<=n-i-1;j++){
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+  for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void pattern10(int n){
+for(int i=1;i<=2*n-1;i++){
+  int stars=i;
+  if(i>n) stars = 2*n-i;
+  for(int j=1;j<=stars;j++){
+    cout<<"*";
+  }
+  cout<<endl;
+}
+}
+void pattern11(int n){
+int start =1;
+for(int i=0;i<n;i++){
+  if(i % 2 == 0) start =1;
+  else start = 0;
+  for(int j=0;j<=i;j++){
+    cout<<start;
+    start = 1-start;
+  }
+  cout<<endl;
+}
+}
+void pattern12(int n){
+int space = 2*(n-1);
+for(int i=1;i<=n;i++){
+  for(int j=1;j<=i;j++){
+    cout<<j;
+    
+  }
+  for(int j = 1;j<=space;j++){
+    cout<<" ";
+  }
+  for(int i=1;i>=1;j--){
+    cout<<j;
+  }
+  cout<<endl;
+  space -=2;
+}
+}
+void pattern13(int n){
+int num = 1;
+for(int i=0;i<n;i++){
+  for(int j=0;j<=i;j++){
+    cout<<num;
+    num+=1;
+  }
+  cout<<endl;
+}
+}
+void pattern14(int n){
+
+}
+
+void pattern15(int n){
+
+}
+void pattern16(int n){
+  
+}
+
 
 int main() {
     int n =5;
@@ -73,5 +173,14 @@ int main() {
     pattern5(n);
     pattern6(n);
     pattern7(n);
+    pattern8(n);
+    pattern9(n);
+    pattern10(n);
+    pattern11(n);
+    pattern12(n);
+    pattern13(n);
+    pattern14(n);
+    pattern15(n);
+    
     return 0;
 }
